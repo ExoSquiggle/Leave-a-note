7810696f2824934d4e74d18e34578e2e864aa391
ExoSquiggle <iroc-z180272@Hotmail.com>
2019-03-11 18:06:37 -0700

    Update settings.py

--- leave_a_note/settings.py
+++ leave_a_note/settings.py
@@ -18,13 +18,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'k-yt*(ztqp4%j4z90_@2+wjvbkg@vdx^v!g(yb6s3=c(hsd5z#'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
 ALLOWED_HOSTS = []
 
 
